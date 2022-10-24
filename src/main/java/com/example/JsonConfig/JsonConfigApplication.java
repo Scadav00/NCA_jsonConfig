6906db13ede02092199021c61391d65dec7d8b3c
FilipOruzinsky <oruzinskyfilip@gmail.com>
2022-10-24 15:31:53 +0200

    copy_from_previous_project

--- src/main/java/com/example/JsonConfig/JsonConfigApplication.java
+++ src/main/java/com/example/JsonConfig/JsonConfigApplication.java
@@ -10,6 +10,30 @@ public class JsonConfigApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(JsonConfigApplication.class, args);
 		System.out.println("Welcome to Gson !");
+
+		//serializeConfigSimple();
+		deserializeConfigSimple();
 	}
 
+
+	private static void serializeConfigSimple() {
+
+		Plotly config = new Plotly(
+				"0.0.0.0",
+				8051,
+				true,
+				"KBA - Dashboard"
+		);
+		Gson gson = new Gson();
+		String json = gson.toJson(config);
+
+	}
+
+	private static void deserializeConfigSimple() {
+		String configJson = "{'host':'0.0.0.0','port':8051,'debug':true,'title':'KBA - Dashboard'}";
+
+		Gson gson = new Gson();
+		Plotly configSimple = gson.fromJson(configJson, Plotly.class);
+
+	}
 }
---
+++ src/main/java/com/example/JsonConfig/Plotly.java
@@ -0,0 +1,18 @@
+package com.example.JsonConfig;
+
+public class Plotly {
+    private String host;
+    private int port;
+    private boolean debug;
+    private String title;
+
+    public Plotly(String host, int port, boolean debug, String title) {
+        this.host = host;
+        this.port = port;
+        this.debug = debug;
+        this.title = title;
+    }
+
+}
+
+}
