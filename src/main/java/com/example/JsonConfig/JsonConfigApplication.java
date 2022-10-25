ba5901769116c55c7978f2f6e8f644a418e71c1c
Scadav00 <davidscava@gmail.com>
2022-10-25 15:32:35 +0200

    jsonPath dependency added, edit works with config.json, read of edited fiel also

--- src/main/java/com/example/JsonConfig/JsonConfigApplication.java
+++ src/main/java/com/example/JsonConfig/JsonConfigApplication.java
@@ -1,10 +1,13 @@
 package com.example.JsonConfig;
 
 import com.google.gson.Gson;
+import com.jayway.jsonpath.JsonPath;
+import lombok.SneakyThrows;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
+import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 
@@ -15,11 +18,22 @@ public class JsonConfigApplication {
 		SpringApplication.run(JsonConfigApplication.class, args);
 		System.out.println("Welcome to Gson !");
 
-		//serializeConfigSimple();
-		deserializeConfigNested();
+//		serializeConfigSimple();
+//		deserializeConfigNested();
+		editJson();
 	}
 
 //no exception implemented !
+
+	private static void editJson() throws IOException {
+		String file = "/home/ds/IdeaProjects/JsonConfig/src/main/java/com/example/JsonConfig/Config.json";
+		String json = new String(Files.readAllBytes(Paths.get(file)));
+//		String pathToString = "$.plotly.boxes.name1";
+		String newJson = JsonPath.parse(json).set( "$.es.srv.index", "test").jsonString();
+		Files.write(Paths.get(file), newJson.getBytes());
+		System.out.println(json);
+	}
+
 	private static void serializeConfigNested() {
 		Plotly configSimple = new Plotly(
 				"0.0.0.0",
@@ -43,7 +57,7 @@ public class JsonConfigApplication {
 
 //        String jsonConfig = "{ 'plotly':{ 'host':'0.0.0.0', 'port':8051, 'debug':true, 'title':'KBA - Dashboard', 'boxes':{ 'name1':'1', 'name2':'2', 'name3':'3', 'name4':'4'}}}";
 		Config config = new Gson().fromJson(jsonConfig, Config.class);
-
+//at the moment it does create an object but doesnt read
 	}
 
 	public static String readFileAsString(String file) throws Exception
