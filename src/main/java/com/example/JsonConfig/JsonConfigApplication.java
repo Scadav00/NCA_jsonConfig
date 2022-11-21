112a9e59a904167ebccd6f14eeadd7004f4baca3
FilipOruzinsky <oruzinskyfilip@gmail.com>
2022-11-21 15:51:37 +0100

    method outputCompare created

--- src/main/java/com/example/JsonConfig/JsonConfigApplication.java
+++ src/main/java/com/example/JsonConfig/JsonConfigApplication.java
@@ -2,15 +2,19 @@ package com.example.JsonConfig;
 
 import com.google.gson.Gson;
 import com.jayway.jsonpath.JsonPath;
+import com.sun.istack.NotNull;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.util.SerializationUtils;
 
+
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.Map;
+
+
 
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class JsonConfigApplication {
@@ -22,19 +26,61 @@ public class JsonConfigApplication {
 //		editJson();
 //		readJson();
 //		mapJson();
-		mapSerialization();
+//		mapSerialization();
+//		mapDeserialization();
+		outputCompare();
 
 //		String file = "/home/ds/IdeaProjects/JsonConfig/src/main/java/com/example/JsonConfig/Config.json";
 //		String json = new String(Files.readAllBytes(Paths.get(file)));
 //		convertJsonIntoMap(json);
 	}
 
-	public static void  mapSerialization() throws IOException{
-		Map<String,Object> configMap = mapJson();
+
+	@NotNull
+	public static byte[] mapSerialization() throws IOException {
+		Map<String, Object> configMap = mapJson();
 		byte[] data = SerializationUtils.serialize(configMap);
 		System.out.println(data);
-//		return data;
+		return data;
 	}
+
+	public static Object mapDeserialization() throws IOException {
+//		if (objectData == null) {
+//			throw new IllegalArgumentException("The byte[] must not be null");
+//		}
+		byte[] configMap = mapSerialization();
+		Object data = SerializationUtils.deserialize(configMap);
+		//System.out.println(data);
+		return data;
+	}
+
+	public static void outputCompare() throws IOException {
+		Map<String, Object> configMap = mapJson();
+		String one = SerializationUtils.serialize(configMap).toString();
+
+		byte[] configMap2 = mapSerialization();
+		String two = SerializationUtils.deserialize(configMap2).toString();
+
+		System.out.println(one.equals(two));
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //	public static Map<String, Object> mapJson() throws IOException {
 	public static Map<String, Object> mapJson() throws IOException {
